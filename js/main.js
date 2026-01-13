c639db8e091644cefcd2a6e1617087b17cdd46b5
listenbridgecn <listenbridge@outlook.com>
2026-01-13 17:13:58 +0800

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// 导航高亮
+document.addEventListener('DOMContentLoaded', () => {
+  const links = document.querySelectorAll('nav ul li a');
+  links.forEach(link => {
+    if(link.href === window.location.href){
+      link.classList.add('active');
+    }
+  });
+});
